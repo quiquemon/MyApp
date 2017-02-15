e6864fc27e53c0fa16cdf5abb82e6593b9bf5261
PROGRAMACION2 <PROGRAMACION2@LAPTOP-PT7LJ4TU>
2017-02-15 14:13:19 -0600

    1. Controladores y Rutas

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,10 @@
+class IndexController < ApplicationController
+	def index
+	end
+
+	def about_us
+	end
+
+	def more_info
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root to: 'index#index'
+	
+	get 'index',     to: 'index#index'
+	get 'about_us',  to: 'index#about_us'
+	get 'more_info', to: 'index#more_info'
 end
